a26cc71ab82f92f52e535eedf849436f2dda6445
Piyush <piyushranjan2807@gmail.com>
2024-01-15 13:40:13 +0530

    Operators 1.1

--- Basic C Programs/03_Operators.c
+++ Basic C Programs/03_Operators.c
@@ -5,6 +5,13 @@ int main(){
     int b = 4;
     printf("%d\n",a+++b);//This will evaluate a's post increment and then add it to b i.e., firstly a and b will be added and then a will be increased
     printf("a = %d\nb = %d\n",a,b);
+
+    int i = (a<b) && (++b);
+    printf("%d\n%d\n",i,b);// b = 4 because it first evaluates the left side expression and if it turns out to be false, it stops
+
+    i = (a>b) && (++b);
+    printf("%d\n%d\n",i,b);
     
+//same is with the '||' operators. If the first expression is true then rest of the expression is not evaluated
     return 0;
 }
